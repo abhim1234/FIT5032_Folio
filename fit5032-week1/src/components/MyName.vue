449d79cebfad3e6ecb704220ad34bbbfd48431cb
Abhimanyu Rajvanshi <abhimanyurajvanshi30@gmail.com>
2024-08-01 07:26:09 +0530

    Adding the initial project created in week 1 for FIT5032 with additional MyName.vue file

---
+++ fit5032-week1/src/components/MyName.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <h1>Welcome, Abhimanyu Rajvanshi!</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MyName'
+  }
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: #42b983;
+  }
+  </style>
+  
