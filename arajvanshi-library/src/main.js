15468379a10553d10a5604a2216e80bb7d4ac5b9
Abhimanyu Rajvanshi <abhimanyurajvanshi30@gmail.com>
2024-08-13 07:32:37 +0530

    Week 4 Studio tasks for Validating Form with HTML & JavaScript along with PrimeVue DataTable implementation

--- arajvanshi-library/src/main.js
+++ arajvanshi-library/src/main.js
@@ -1,9 +1,24 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-// import './style.css'
 // import bootstrap css
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-createApp(App).mount('#app')
+import PrimeVue from 'primevue/config';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import Aura from '@primevue/themes/aura';
+
+const app = createApp(App);
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura
+  }
+});
+
+// Register PrimeVue components globally
+app.component('DataTable', DataTable);
+app.component('Column', Column);
+
+app.mount("#app");
