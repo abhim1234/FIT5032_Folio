da3524f9bb6943ea3da222954bf12f68ac0297cb
Abhimanyu Rajvanshi <abhimanyurajvanshi30@gmail.com>
2024-09-09 08:51:17 +0530

    Week 8 - Firestore Database Completed

--- NoMash-Library/src/main.js
+++ NoMash-Library/src/main.js
@@ -1,24 +1,8 @@
+// main.js
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDfmdoV9Hh1BKf8qiClZW2QJ0nVvtsYWf4",
-  authDomain: "week7-abhimanyu.firebaseapp.com",
-  projectId: "week7-abhimanyu",
-  storageBucket: "week7-abhimanyu.appspot.com",
-  messagingSenderId: "1055195233032",
-  appId: "1:1055195233032:web:c40b4aff6fa26a21c58abd"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
+import { auth, db } from './firebase/init'; // Import Firebase services from init.js
 
 // Initialize Vue Application
 const vueApp = createApp(App);
